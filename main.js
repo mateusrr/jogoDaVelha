761c623b4daeb6e966be628db8ef3240945ee7d5
mateus <mateus046@gmail.com>
2022-10-25 13:47:25 -0300

    update

--- main.js
+++ main.js
@@ -15,51 +15,51 @@ let posicoes = [
 ];
 
 function init() {
-  selecionado = [];
+selecionado = [];
 
-  jogadorDaVez.innerHTML = `Jogador da vez: ${jogador}`;
+jogadorDaVez.innerHTML = `Jogador da vez: ${jogador}`;
 
-  document.querySelectorAll(".jogo button").forEach((item) => {
+document.querySelectorAll(".jogo button").forEach((item) => {
     item.innerHTML = "";
     item.addEventListener("click", newMove);
-  });
+});
 }
 
 init();
 
 function newMove(e) {
-  const index = e.target.getAttribute("data-i");
-  e.target.innerHTML = jogador;
-  e.target.removeEventListener("click", newMove);
-  selecionado[index] = jogador;
+const index = e.target.getAttribute("data-i");
+e.target.innerHTML = jogador;
+e.target.removeEventListener("click", newMove);
+selecionado[index] = jogador;
 
-  setTimeout(() => {
-    check();
-  }, [100]);
+setTimeout(() => {
+  check();
+}, [100]);
 
-  jogador = jogador === "X" ? "O" : "X";
-  jogadorDaVez.innerHTML = `Jogador da vez: ${jogador}`;
+jogador = jogador === "X" ? "O" : "X";
+jogadorDaVez.innerHTML = `Jogador da vez: ${jogador}`;
 }
 
 function check() {
-  let playerMove = jogador === "X" ? "O" : "X";
+let playerMove = jogador === "X" ? "O" : "X";
 
-  const items = selecionado
-    .map((item, i) => [item, i])
-    .filter((item) => item[0] === playerMove)
-    .map((item) => item[1]);
+const items = selecionado
+  .map((item, i) => [item, i])
+  .filter((item) => item[0] === playerMove)
+  .map((item) => item[1]);
 
-  for (pos of posicoes) {
-    if (pos.every((item) => items.includes(item))) {
-      alert("O jogador " + playerMove + " ganhou!");
-      init();
-      return;
-    }
-  }
-
-  if (selecionado.filter((item) => item).length === 9) {
-    alert("Ninguém ganhou, jogue novamente!");
+for (pos of posicoes) {
+  if (pos.every((item) => items.includes(item))) {
+    alert("O jogador " + playerMove + " ganhou!");
     init();
     return;
   }
+}
+
+if (selecionado.filter((item) => item).length === 9) {
+  alert("Ninguém ganhou, jogue novamente!");
+  init();
+  return;
+}
 }
